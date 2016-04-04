bb1e6a2a0c5bf182063ae9188f1fc7f65fcb7545
Nicolas Capiaumont <nicolas.capiaumont@gmail.com>
2016-04-04 23:39:53 +0200

    fix
    * Premier commit du projet android Malo
    
    * fix project
    
    * Ajout des premières pages de l'application
    
    * Mise en place des premières pages de l'application, avec différentes activtés
    
    * suppr restriction des iml
    
    * Fix android project
    
    * Fix project omg
    
    * update gitignore
    
    * Update gitignore
    
    * .gitignore is now working
    
    * ajout de nouvelles pages
    
    * fix project

--- app/src/main/java/com/ihm/malo/CreateAccountActivity.java
+++ app/src/main/java/com/ihm/malo/CreateAccountActivity.java
@@ -2,6 +2,8 @@ package com.ihm.malo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class CreateAccountActivity extends AppCompatActivity {
 
@@ -9,5 +11,14 @@ public class CreateAccountActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_account);
+
+        TextView inscription = (TextView) findViewById(R.id.inscriptionButton);
+        inscription.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //change to activity_no_family
+                setContentView(R.layout.activity_no_family);
+            }
+        });
     }
 }
